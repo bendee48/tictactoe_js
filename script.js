3c8df63491754272084c459656c99c6488189d7d
Ben Daniels <benbanj@hotmail.com>
2021-08-12 22:05:13 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log('working!')
+
+function player(name) {
+  return { name }
+}
+
+const gameboard = (() => {
+  let board = [['O', 'X', 'O'],['X', 'O', 'X'],['O', 'X', 'O']];
+
+  return { board }
+})()
+
+const gameEngine = (() => {
+
+})()
+
+const displayController = (() => {
+  
+})()
