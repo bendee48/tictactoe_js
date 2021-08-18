3767aa24a367d003335e1c4c926c80a762e010c1
Ben Daniels <benbanj@hotmail.com>
2021-08-18 13:56:07 +0100

    Setting up game board and display

--- script.js
+++ script.js
@@ -10,10 +10,20 @@ const gameboard = (() => {
   return { board }
 })()
 
-const gameEngine = (() => {
-
-})()
 
 const displayController = (() => {
+  const displayBoard = (board, boardElement) => {
+    boardElement.innerHTML = `<div>${board}</div><div>hello</div>`
+  }
+  
+  return {displayBoard}
+})()
+
+const gameEngine = (() => {
+  // Only really want to initalize boards etc once, not in individual functions
+  const board = gameboard.board;
+  const boardElement = document.querySelector('.board');
+
+  displayController.displayBoard(board, boardElement);
   
 })()
