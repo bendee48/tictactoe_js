3bec012aca7d145d3357266af359497fc564f27c
Ben Daniels <benbanj@hotmail.com>
2021-09-15 13:26:28 +0100

    Adding a check win function

--- script.js
+++ script.js
@@ -28,18 +28,39 @@ const players = (() => {
 
 const gameLogic = (() => {
   const fillSquare = (e) => {
-    let [idx1, idx2] = e.target.dataset.key;
+    const coords = e.target.dataset.key;
+    let [idx1, idx2] = coords;
     if (gameboard.board[idx1][idx2] === '') {
       gameboard.board[idx1][idx2] = players.active().mark;
     } else {
       return;
     }
+    //MOve to game engine??? Maybe??! YAS DEFINITLEY OR SEPERATE FUNCTION
+    win(coords, players.active().mark)
     players.switchPlayer();
     displayController.displayBoard();
     //UNCOUPLE DISPLAY FROM HERE MAYBE
   }
 
-  return { fillSquare }
+  const win = (coords, playerMark) => {
+    // COULD JUST CHECK EVERY ROW AFTER EACH ROUND
+    //CAN SKIP FIRST 4 turns
+    // object showing winning moves from that squae, check those
+    const winRows = { '00': [['00', '01', '02'], ['00', '11', '22'], ['00', '10', '20']],
+                      '01': [['00', '01', '02'], ['01', '11', '21']],
+                      '02': [['00', '01', '02'], ['02', '11', '20'], ['02', '12', '22']],
+                      '10': [['00', '10', '20'], ['10', '11', '12'],],
+                      '11': [['00', '11', '22'], ['02', '11', '20'], ['10', '11', '12'], ['01', '11', '21']],
+                      '12': [['10', '11', '12'], ['02', '12', '22']],
+                      '20': [['00', '10', '20'], ['02', '11', '20'], ['20', '21', '22']],
+                      '21': [['01', '11', '21'], ['20', '21', '22']],
+                      '22': [['00', '11', '22'], ['20', '21', '22'], ['02', '12', '22']] };
+    const board = gameboard.board;
+
+    //CHECK ROWS FOR WINS with playermark and gameboard
+  }
+
+  return { fillSquare, win }
 })()
 
 
@@ -58,7 +79,8 @@ const interactionListener = (() => {
 })()
 
 const displayController = (() => {
-  const displayBoard = (board, boardElement) => {
+  //initialize save board here?? MEMOMIZE BABAY
+  const displayBoard = () => {
     const squares = elementSelector.squares
     squares.forEach(square => {
       let [idx1, idx2] = square.dataset.key.split('');
@@ -71,11 +93,6 @@ const displayController = (() => {
 
 
 const gameEngine = (() => {
-  // Only really want to initalize boards etc once, not in individual functions
-  const board = gameboard.board;
-  const boardElement = document.querySelector('.board');
-
-
-  displayController.displayBoard(board, boardElement);
+  displayController.displayBoard();
 })()
 
