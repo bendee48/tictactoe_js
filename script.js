d56c5814617f83d3004c82335530930b4b2a8ce0
Ben Daniels <benbanj@hotmail.com>
2021-09-23 21:30:35 +0100

    Adding to form

--- script.js
+++ script.js
@@ -21,18 +21,21 @@ const eventObserver = (() => {
 
 const gameboard = (() => {
   let board = [['', '', ''],['', '', ''],['', '', '']];
-
-  return { board }
+  
+  const getBoard = () => {
+    return board;
+  }
+  return { getBoard }
 })()
 
-const player = function(name, mark) {
-  return { name, mark }
+const createPlayer = function({ name, symbol }) {
+  return { name, symbol }
 }
  
 const players = (() => {
-  const player1 = player('Player 1', 'X');
-  const player2 = player('Player 2', 'O');
-  const both = [player1, player2];
+  let player1 = createPlayer({name: 'Player 1', symbol: 'X'});
+  let player2 = createPlayer({ name: 'Player 2', symbol: 'O'});
+  let both = [player1, player2];
 
   const active = () => {
     return both[0];
@@ -42,29 +45,39 @@ const players = (() => {
     both.reverse();
   }
 
-  return { both, player1, player2, active, switchPlayer }
+  return { player1, player2, active, switchPlayer }
 })()
+
+const gameSetup = (() => {
+  const savePlayers = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+  }
+  // IMPLEMENT FUNCTION STUFF 
+  return { savePlayers }
+})();
 
 const gameLogic = (() => {
   const fillSquare = (e) => {
     const coords = e.target.dataset.key;
     let [idx1, idx2] = coords;
-    if (gameboard.board[idx1][idx2] === '') {
-      gameboard.board[idx1][idx2] = players.active().mark;
-      eventObserver.run('check win', coords, players.active().mark)
+    if (gameboard.getBoard()[idx1][idx2] === '') {
+      gameboard.getBoard()[idx1][idx2] = players.active().symbol;
+      // Run functions signed up to Check win and Update Board
+      eventObserver.run('check win', coords, players.active().symbol)
       eventObserver.run('update board')
     } else {
       return;
     }
   }
 
-  const hasWon = (coords, playerMark) => {
-    if (checkWin(coords, playerMark)) {
+  const hasWon = (coords, playerSymbol) => {
+    if (checkWin(coords, playerSymbol)) {
       displayController.displayWin();
     }
   }
 
-  const checkWin = (coords, playerMark) => {
+  const checkWin = (coords, playerSymbol) => {
     //CAN SKIP FIRST 4 turns save a little time
     // object showing winning moves from that square, check only those
     const winRows = { '00': [['00', '01', '02'], ['00', '11', '22'], ['00', '10', '20']],
@@ -76,13 +89,13 @@ const gameLogic = (() => {
                       '20': [['00', '10', '20'], ['02', '11', '20'], ['20', '21', '22']],
                       '21': [['01', '11', '21'], ['20', '21', '22']],
                       '22': [['00', '11', '22'], ['20', '21', '22'], ['02', '12', '22']] };
-    const board = gameboard.board;
+    const board = gameboard.getBoard();
 
     // Check possible winning rows for active player's mark 
     return winRows[coords].some(row => {
       return row.every(coord => {
         let [idx1, idx2] = coord;
-        return board[idx1][idx2] === playerMark;
+        return board[idx1][idx2] === playerSymbol;
       });
     });
   }
@@ -93,8 +106,9 @@ const gameLogic = (() => {
 
 const elementSelector = (() => {
   const squares = document.querySelectorAll('.square');
+  const playerForm = document.querySelector('#playerForm');
   
-  return { squares }
+  return { squares, playerForm }
 })()
 
 
@@ -103,6 +117,8 @@ const interactionListener = (() => {
   elementSelector.squares.forEach(square => {
     square.addEventListener('click', gameLogic.fillSquare);
   });
+
+  elementSelector.playerForm.addEventListener('submit', gameSetup.savePlayers);
 })()
 
 const displayController = (() => {
@@ -111,7 +127,7 @@ const displayController = (() => {
     const squares = elementSelector.squares
     squares.forEach(square => {
       let [idx1, idx2] = square.dataset.key.split('');
-      square.innerHTML = gameboard.board[idx1][idx2];
+      square.innerHTML = gameboard.getBoard()[idx1][idx2];
     });
   }
 
@@ -125,6 +141,7 @@ const displayController = (() => {
 
 const gameEngine = (() => {
   displayController.displayBoard();
+  // Subscribe functions to Event Observer to be run later
   eventObserver.subscribe('check win', gameLogic.hasWon) //Check for win after each successful move
   eventObserver.subscribe('update board', players.switchPlayer); // Switch player after successful move
   eventObserver.subscribe('update board', displayController.displayBoard) // Re-render board after each turn
